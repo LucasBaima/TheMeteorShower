ee146b3976d241f732ec80a0538b42962408c3f8
joãoLucas <jlogb@cesar.school>
2026-05-07 18:49:03 -0300

    "feat: implementação loop infinito, captura de input e controle de FPS"

--- main.c
+++ main.c
@@ -1,10 +1,31 @@
+#include <stdio.h>
 #include <stdlib.h>
+#include <windows.h>
+#include <conio.h>
 #include "game.h"
 
+void processarInput() {
+    if (_kbhit()) {
+        char tecla = _getch();
+        if ((tecla == 'a' || tecla == 'A') && jogador.x > 0) jogador.x--;
+        if ((tecla == 'd' || tecla == 'D') && jogador.x < LARGURA - 1) jogador.x++;
+        if (tecla == 'q' || tecla == 'Q') gameOver = 1;
+    }
+}
+
+
+
+
 int main() {
-    system("cls");
     inicializarJogo();
-    prepararMatriz();
-    desenharTela();
+    system("cls");
+
+
+    while (!gameOver){
+        processarInput();
+        prepararMatriz();
+        desenharTela();
+        Sleep(50);
+    }
     return 0;
 }
